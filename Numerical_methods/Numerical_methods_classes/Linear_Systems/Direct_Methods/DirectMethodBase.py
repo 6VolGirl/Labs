4d1fe12765f4a31f6f444242f1c63a119bc73e7b
VolGirl <6anna@tut.by>
2026-05-06 23:14:19 +0300

    Base method for direct methods

---
+++ Numerical_methods/Numerical_methods_classes/Linear_Systems/Direct_Methods/DirectMethodBase.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+from BaseMethod import NumericalMethod
+
+class DirectMethodBase(NumericalMethod):
+    """
+    Базовый класс для прямых методов решения
+    (аналитическое или одношаговое точное решение задачи).
+    """
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.method_type = "direct"
+
+    def solve(self, *args, **kwargs):
+        """
+        Интерфейс решения задачи.
+        Должен быть переопределён в наследниках.
+        """
+        raise NotImplementedError(
+            f"Метод solve() не реализован в классе {self.__class__.__name__}"
+        )
